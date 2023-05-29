a0e33544702df87c6b2bd43dc949facb83e70b69
FabioV123 <staar54747@gmail.com>
2023-05-29 17:40:54 -0300

    03

--- 28.py
+++ 28.py
@@ -1,6 +1,6 @@
-import random
+from random import randint
 n1= int (input('Digite um número de 1 a 5: '))
-n2= random.randint(1,5)
+n2= randint(1,5)
 if n1 == n2:
     print('Você acertou! ')
 else:
