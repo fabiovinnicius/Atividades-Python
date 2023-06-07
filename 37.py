6dcf487760ce4965a76f07add614a1ac2dbc0428
FabioV123 <staar54747@gmail.com>
2023-06-07 17:08:16 -0300

    05

--- 37.py
+++ 37.py
@@ -1,1 +1,12 @@
-print('tentando...')
+num= int(print('Digite um numero inteiro: '))
+print('''Escolha uma das bases para conversão: 
+[ 1 ] converter para BINÁRIO
+[ 2 ] converter para OCTAL
+[ 3 ] converter para HEXADECIMAL''')
+opção= int(input('Sua opção é: '))
+if opção == 1:
+    print('{} convertido para BINÁRIO é: '.format(num, bin(num)))
+elif opção == 2:
+    print('{} convertido para OCTAL é: '.format(num, oct(num)))
+elif opção == 3:
+    print('{} convertido para HEXADECIMAL é: '.format(num, hex(num)))
