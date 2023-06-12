812b4f585d2b074020af72496719da9a8bd13cf4
FabioV123 <staar54747@gmail.com>
2023-06-12 09:31:21 -0300

    06

---
+++ 40.py
@@ -0,0 +1,6 @@
+n1= float (input('Digite sua primeira nota '))
+n2= float(input('Digite sua segunda nota '))
+media= (n1+n2) / 2
+if media < 5.0: print('Você foi REPROVADO! ')
+elif 5.0 <= media <= 6.9: (print('Você está de recuperação'))
+else: (print('Você foi APROVADO!'))
